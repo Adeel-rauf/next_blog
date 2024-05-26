7201ebddf25d9a3fe07e0aa2cec16c562494803f
adeel-rauf <adeel@psm@gmail.com>
2024-05-26 14:20:52 +0500

    ignored lint

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,13 @@ const nextConfig = {
                 port:""
             }
         ]
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
+    
 };
 
 export default nextConfig;
