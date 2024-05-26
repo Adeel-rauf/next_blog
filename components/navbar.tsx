8472d0b7f2952c51ead435fe1ae0acee2efa55e7
adeel-rauf <adeel@psm@gmail.com>
2024-05-26 13:30:26 +0500

    blog project

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,12 +12,12 @@ export default function Navbar() {
         <Link href={"/"} className="">
          <motion.div  
            className="text-6xl font-bold ">
-            <motion.span initial={{y:-1000,opacity:0}} whileInView={{y:0,opacity:1}} transition={{delay:0.3,duration:0.5}} className="inline">Blog</motion.span>
-            <motion.span initial={{y:-1000,opacity:0}} whileInView={{y:0,opacity:1}} transition={{delay:0.4,duration:0.5,}}
+            <motion.span initial={{y:-1000,opacity:0}} animate={{y:0,opacity:1}} transition={{delay:0.3,duration:0.5}} className="inline">Blog</motion.span>
+            <motion.span initial={{y:-1000,opacity:0}} animate={{y:0,opacity:1}} transition={{delay:0.4,duration:0.5,}}
              className="text-primary inline">Fest</motion.span>
          </motion.div>
         </Link>
-           <motion.span initial={{y:-30,opacity:0}} whileInView={{y:0,opacity:1}} 
+           <motion.span initial={{y:-30,opacity:0}} animate={{y:0,opacity:1}} 
            transition={{delay:1.5, duration:1,type:"spring",stiffness:350}}
            whileHover={{scale:1.1}}
             >
@@ -30,7 +30,7 @@ export default function Navbar() {
         
            </div>
            <motion.h2 
-           initial={{x:-70,opacity:0}} whileInView={{x:0,opacity:1}} transition={{delay:1.1, duration:3,type:"spring",stiffness:250}}
+           initial={{x:-70,opacity:0}} animate={{x:0,opacity:1}} transition={{delay:1.1, duration:3,type:"spring",stiffness:250}}
            className=" mt-1 max-w-md sm:max-w-lg mx-auto italic 
            font-semibold font-serif sm:mt-1 sm:text-2xl mb-2 ">A written expression of my thoughts</motion.h2> 
            </>
